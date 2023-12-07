87b03ad85ff0d0de996b983f0a822c3c23ded82a
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-12-07 21:00:58 +0000

    user Schema

--- db/index.js
+++ db/index.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose"
 
 export const connectDB=async ()=>{
    await mongoose.connect("mongodb+srv://gio:e3b3b8454@cluster0.8amxfsj.mongodb.net/")
-    .then(()=>console.log("conectado"))
+    .then(()=>console.log("conectado a la base de datos"))
     .catch((error=>console.log(error)))
 }
---
+++ models/User.js
@@ -0,0 +1,32 @@
+import {Schema,model} from "mongoose";
+
+
+const userSchema=new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        match:[
+            /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ,
+            "provide a email valid"
+        ]
+    },
+    displayName:{
+        type:String,
+        required:true
+    },
+},{
+    timestamps:true,
+    versionKey:false
+})
+
+
+module.export= model("User",userSchema)
